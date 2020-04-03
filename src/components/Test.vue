eed0e91184fd1dc792a338ed25b40e32aa46742c
Priya Geetha <prigre@signicat.com>
2020-04-03 08:57:36 +0200

    First commit

---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h1 v-on:click="changeHeader">{{header}}</h1>
+  </div>
+</template>
+<script>
+
+import { EventBus } from '@/eventBus'
+export default {
+  name: 'Test',
+  props: {
+    header:{
+        type: String
+    } 
+  },
+  methods: {
+      changeHeader (){
+          this.header = "changed header";
+          EventBus.$emit('changeIt', 'changed header');
+      }
+  }
+}
+</script>
---
+++ src/components/Test2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div> <h1>{{header}}</h1></div>
+</template>
+<script>
+import { EventBus } from '@/eventBus';
+export default {
+  name: 'Test2',
+  data(){
+      return{
+          header:"Something"
+      }    
+  },
+  created(){
+    EventBus.$on('changeIt', (data) => {
+      this.header = data;
+    })
+  }
+}
+</script>
